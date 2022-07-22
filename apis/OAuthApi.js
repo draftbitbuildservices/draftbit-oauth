dbd8473f11acc921f22dbf06bf4d2a0275da3f24
Amitkumar Mishra <71927649+AmitkumarMishra-code@users.noreply.github.com>
2022-07-22 21:57:32 +0530

    Create OAuthApi.js

---
+++ apis/OAuthApi.js
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import {
+  useQuery,
+  useMutation,
+  useIsFetching,
+  useQueryClient,
+} from 'react-query';
+import useFetch from 'react-fetch-hook';
+import { useIsFocused } from '@react-navigation/native';
+import usePrevious from '../utils/usePrevious';
+import * as GlobalVariables from '../config/GlobalVariableContext';
+
+export const newEndpointGET = Constants =>
+  fetch(
+    `https://x8ki-letl-twmt.n7.xano.io/api:3gYiP53R/oauth/google/init?redirect_uri=https://auth.expo.io/@draftbitbuildservices/social-auth://HomeScreen/:code/:redirect_uri`,
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then(res => {
+      if (!res.ok) {
+        console.error('Fetch error: ' + res.status + ' ' + res.statusText);
+      }
+      return res;
+    })
+    .then(res => res.json())
+    .catch(() => {});
+
+export const useNewEndpointGET = () => {
+  const Constants = GlobalVariables.useValues();
+  const isFocused = useIsFocused();
+
+  return useFetch(
+    `https://x8ki-letl-twmt.n7.xano.io/api:3gYiP53R/oauth/google/init?redirect_uri=https://auth.expo.io/@draftbitbuildservices/social-auth://HomeScreen/:code/:redirect_uri`,
+    {
+      depends: [isFocused],
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+};
+
+export const FetchNewEndpointGET = ({
+  children,
+  onData = () => {},
+  refetchInterval,
+}) => {
+  const Constants = GlobalVariables.useValues();
+  const isFocused = useIsFocused();
+  const prevIsFocused = usePrevious(isFocused);
+
+  const refetch = () => {};
+  const {
+    isLoading: loading,
+    data,
+    error,
+  } = useFetch(
+    `https://x8ki-letl-twmt.n7.xano.io/api:3gYiP53R/oauth/google/init?redirect_uri=https://auth.expo.io/@draftbitbuildservices/social-auth://HomeScreen/:code/:redirect_uri`,
+    {
+      depends: [isFocused],
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  React.useEffect(() => {
+    if (!prevIsFocused && isFocused) {
+      refetch();
+    }
+  }, [isFocused, prevIsFocused]);
+
+  React.useEffect(() => {
+    if (error) {
+      console.error('Fetch error: ' + error.status + ' ' + error.statusText);
+      console.error(error);
+    }
+  }, [error]);
+  React.useEffect(() => {
+    if (data) {
+      onData(data);
+    }
+  }, [data]);
+
+  return children({ loading, data, error, refetchNewEndpoint: refetch });
+};
